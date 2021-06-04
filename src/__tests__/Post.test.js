03acb17621eb5bac62202a96fe01d3863669dcd4
mflanders06 <mflanders06@gmail.com>
2021-06-03 21:37:42 -0600

    setup test for the post render

---
+++ src/__tests__/Post.test.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { render, act } from '@testing-library/react';
+import Post from '../views/Post';
+import axios from 'axios';
+import { MemoryRouter } from 'react-router-dom';
+import { posts } from './__data__/testData';
+
+test('`Post` should render a post on screen', async () => {
+    const post = posts[0];
+    let container;
+    jest
+        .spyOn(axios, 'get')
+        .mockImplementation(() => Promise.resolve({ data: post }));
+
+    await act(async() => {
+        let renderObj = render(
+            <MemoryRouter>
+                <Post match={{ params: { PostID: 1 } }} />
+            </MemoryRouter>
+        );
+        container = renderObj.container;
+    })
+        
+    expect(container.textContent).toContain(post.text);
+});
+
